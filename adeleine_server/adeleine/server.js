b28a2879898434d6f465367e8d00d29defee8f4d
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-07-06 12:16:54 +0900

    Followed JSLint

--- adeleine_server/adeleine/server.js
+++ adeleine_server/adeleine/server.js
@@ -8,7 +8,7 @@ var schema = require('./schema');
 var imagestore = require('./imagestore');
 
 var server = new ws.Server({port: 8888}, function() {
-    console.log("Adeleine woke up on 0.0.0.0:8888")
+    console.log("Adeleine woke up on 0.0.0.0:8888");
 });
 
 server.broadcast = function(data) {
@@ -18,7 +18,7 @@ server.broadcast = function(data) {
 };
 server.send_random_image_url = function() {
     imagestore.get_random_image_url(function(err, ret) {
-        if (!err && ret){
+        if (!err && ret) {
             server.broadcast(JSON.stringify({imageUrl: ret}));
         }
     });
@@ -27,16 +27,17 @@ server.send_random_image_url = function() {
 
 server.on('connection', function(socket) {
     socket.on('message', function(data) {
+        var message, validated;
         try {
-            var message = JSON.parse(data);
-        } catch(e) {
+            message = JSON.parse(data);
+        } catch (e) {
             socket.send("Invalid request");
-            return
+            return;
         }
-        var validated = jsonschema.validate(message, schema.messageSchema);
-        if (validated.errors.length != 0) {
+        validated = jsonschema.validate(message, schema.messageSchema);
+        if (validated.errors.length !== 0) {
             socket.send("Invalid message");
-            return
+            return;
         }
 
         imagestore.store_image_url(message.imageUrl, function() {
